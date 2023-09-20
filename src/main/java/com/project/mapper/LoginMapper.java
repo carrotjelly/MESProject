b118504defa138ef1e5f9aa27159e2feeee29035
zeemh <zeemh98@gmail.com>
2023-09-20 11:40:46 +0900

    최종

--- src/main/java/com/project/mapper/LoginMapper.java
+++ src/main/java/com/project/mapper/LoginMapper.java
@@ -9,5 +9,20 @@ import com.project.dto.LoginDTO;
 @Mapper
 public interface LoginMapper {
 	LoginDTO login(Map<String , Object> map);
+
+	int insertUser(LoginDTO dto);
 	
+	int UserCheckId(String id);
+		
+	String checkPasswd(String nowPass);
+	
+	int updatePasswd(String newPass);
+	
+	String searchId(String insertId);
+	
+	LoginDTO getUserByUserId(String userId);
+
+	String findId(String userId);
+
+	String searchpartno(String insertpartno);
 }
--- src/main/java/com/project/service/LoginService.java
+++ src/main/java/com/project/service/LoginService.java
@@ -23,4 +23,37 @@ public class LoginService {
 
 		return mapper.login(map);
 	}
+	public int insertUser(LoginDTO dto) {
+		return mapper.insertUser(dto);
+	}
+	public int UserCheckId(String id, String type) {
+		if (type.equals("user")) {
+			return mapper.UserCheckId(id);
+
+		}
+		return 0;
+	}
+
+
+	public boolean checkPasswd(String nowPass) {
+		String dto = mapper.checkPasswd(nowPass);
+			return dto != null;
+		}
+	//아이디 찾기
+	public String findId(String userId) {
+		return mapper.findId(userId);
+	}
+	//비밀번호 변경
+	public int updatePasswd(String newPass) {
+		return mapper.updatePasswd(newPass);
+	}
+	//비밀번호 변경시 아이디 확인
+	public boolean checkId(String insertId) {
+		String dto = mapper.searchId(insertId);
+		return dto != null;
+	}
+	public boolean checkpartno(String insertpartno) {
+		String dto = mapper.searchpartno(insertpartno);
+		return dto != null;
+	}
 }
